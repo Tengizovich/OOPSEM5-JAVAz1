101583aabb25d064083b8539ce566a46bb88e78e
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-02-25 13:14:37 +0300

    Create ICalcModel.java

---
+++ Model/Method/ICalcModel.java
@@ -0,0 +1,16 @@
+public interface ICalcModel {
+ 
+
+ void setX(Integer[]  value);
+
+ void setY(Integer[]  value);
+ 
+ Integer[] getSum();
+
+ Integer[] getSub();
+
+ Integer[] getMult();
+
+ Integer[] getDiv();
+    
+}
